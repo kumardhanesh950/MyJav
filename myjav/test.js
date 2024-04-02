25a59067b082bd0bbd546d3e14f2cc6c7b4d735f
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2024-04-02 06:30:08 +0000

    file config

---
+++ myjav/test.js
@@ -0,0 +1,59 @@
+// const name ="Dhanesh";
+const fullName ="Dhanesh kumar";
+//console.log(name + fullName +" Khatri")
+//The above method is old , in latest javascript use the syntax
+// console.log(`hello my name is ${fullName.toUpperCase()} but people call me ${name.toUpperCase()} i belong to a Khatri family `)
+
+// console.log(fullName [3]) 
+//here we are asking 3rd positon of string "Dhanesh" p konsa latter he? ans will be => n
+
+let myname ="Dhanesh";
+//Trim will remove the extra spaces 
+
+// console.log(myname.trim()); 
+// console.log(myname.trimStart());
+// console.log(myname.trimEnd())
+
+//charAt will show the position of later in  string "Dhanesh"
+//console.log(name.charAt(1));  here the ans will be h because here counting starts with 0 1 2 3...
+
+//indexOf will show the index number of a in given string
+// console.log(myname.indexOf('a')); 
+//*substring
+// const newstring = myname.substring(0,5)
+// console.log(newstring)
+
+//* slice =>  in Slice we (3,5) means select 5 laters (Dhane) of string "Dhanesh" and 3 here means delete 3 of 5 laters and gives the outback ne
+// const another =myname.slice(3,5);
+// console.log(another) 
+
+//***********MATH ************
+// console.log(Math) 
+
+//.mathabs changes the -ve value in to +ve
+// console.log(Math.abs(-5)); 
+
+//Math.round change the roundof of the given value
+// console.log(Math.round(2.66));
+
+//Math.min gives the minimum value in array
+// console.log(Math.min(3,5,7,1,0.6))
+
+//Math.max gives the maximum value in array
+// console.log(Math.max(3,5,7,1,0.6))
+
+//Math.ceil mean agr value 4 se uper ho like 4.1 or 4.5 etc the value ko round of kerke 5 kerdo
+// console.log(Math.ceil(4.1));
+
+//Math.floor mean agr value 4 se uper ho like 4.1 or 4.5  or 4.9 etc tab bi value ko round of kerke 4 kerdo
+// console.log(Math.floor(4.1));
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toTimeString())
+
+
+
